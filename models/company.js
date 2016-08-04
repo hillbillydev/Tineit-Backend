b902196e46320692dc8c434bb65ed491f3505e83
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-04 15:47:40 +0200

    Company modell.

---
+++ models/company.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    name: { type: String, required: true },
+    secret: { type: String, required: true },
+    user: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = mongoose.model('Company', schema);

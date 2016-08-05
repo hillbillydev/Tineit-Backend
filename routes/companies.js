18428846444a1c110eff03c654c78e9e8573cdbd
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 08:28:55 +0200

    Routes nearly fixed.

--- routes/companies.js
+++ routes/companies.js
@@ -3,27 +3,37 @@ var router = express.Router();
 var passwordHash = require('password-hash');
 
 var Company = require('../models/company');
+var User = require('../models/user');
 
 router.post('/', function (req, res, next) {
-    var company = new Company({
-        name: req.body.name,
-        secret: passwordHash.generate(req.body.secret)
-    });
+    var decoded = jwt.decode(req.query.token);
 
-    company.save(function (err, result) {
+    User.findById(decoded.user._id, function (err, doc) {
         if (err) {
             return res.status(404).json({
-                title: 'An error occured',
+                title: 'An error occurred',
                 error: err
             });
         }
 
-        res.status(200).json({
-            message: 'Success',
-            obj: result
-        })
+        var company = new Company({
+            name: req.body.name,
+            secret: passwordHash.generate(req.body.secret),
+            user: doc
+        });
+        company.save(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            })
+        });
     });
-
 });
 
 module.exports = router;

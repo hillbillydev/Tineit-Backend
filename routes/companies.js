f714d9465dd924a5f562ea15a0188bedf3d876b6
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 20:59:59 +0200

    Fixed error handling for companies get all function.

--- routes/companies.js
+++ routes/companies.js
@@ -6,7 +6,7 @@ var jwt = require('jsonWebToken');
 var Company = require('../models/company');
 var User = require('../models/user');
 
-router.post('/',  function (req, res, next) {
+router.post('/', function (req, res, next) {
     var decoded = jwt.decode(req.headers.authorization);
 
     User.findById(decoded.user._id, function (err, doc) {
@@ -39,10 +39,18 @@ router.post('/',  function (req, res, next) {
 router.get('/', function (req, res, next) {
     var decoded = jwt.decode(req.headers.authorization);
 
- Company.find({ 'user._id': decoded.user._id }).exec(function (err, docs) {
-    return res.json (docs);
-  });
-
+    Company.find({ 'user._id': decoded.user._id }).exec(function (err, docs) {
+        if(err){
+            return res.status(400).json({
+                title: 'An error occurred',
+                    error: err
+            });
+        }
+        res.status(200).json({
+            message: 'Success',
+            obj: docs
+        });
+    });
 });
 
 module.exports = router;

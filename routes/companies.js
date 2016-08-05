93704dc48e79008f45720d669e45269f489cf6de
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 21:53:14 +0200

    Added delete method to companies.

--- routes/companies.js
+++ routes/companies.js
@@ -40,16 +40,56 @@ router.get('/', function (req, res, next) {
     var decoded = jwt.decode(req.headers.authorization);
 
     Company.find({ 'user': decoded.user._id }).exec(function (err, docs) {
-        if(err){
+        if (err) {
             return res.status(400).json({
                 title: 'An error occurred',
-                    error: err
+                error: err
             });
         }
         res.status(200).json({
             message: 'Success',
             obj: docs
         });
+    });
+});
+
+router.delete('/:id', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    Company.findById(req.params.id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        if (!doc) {
+            return res.status(404).json({
+                title: 'No company found',
+                error: { message: 'Company could not be found' }
+            });
+        }
+
+        if (doc.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authorized',
+                error: { message: 'Company created by other user' }
+            });
+        }
+
+        doc.remove(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        })
     });
 });
 

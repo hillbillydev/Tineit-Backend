4845c2b7648626d3a3aa1c628069f3d314ec3784
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 21:21:55 +0200

    Fixed unnecessary error handling.

--- routes/techniques.js
+++ routes/techniques.js
@@ -8,12 +8,6 @@ var User = require('../models/user');
 router.post('/', function (req, res, next) {
     var decoded = jwt.decode(req.headers.authorization);
 
-    if (!decoded) {
-        return res.status(401).json({
-            title: 'Not Authorized'
-        })
-    }
-
     User.findById(decoded.user._id, function (err, doc) {
         if (err) {
             return res.status(404).json({
@@ -42,5 +36,67 @@ router.post('/', function (req, res, next) {
         });
     })
 });
+
+router.get('/', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    Technique.find({ 'user': decoded.user._id }).exec(function (err, docs) {
+        if (err) {
+            return res.status(400).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        res.status(200).json({
+            message: 'Success',
+            obj: docs
+        });
+    })
+});
+
+router.patch('/:id', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    Technique.findById(req.params.id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!doc) {
+            return res.status(404).json({
+                title: 'No technique found',
+                error: { message: 'Technique could not be found' }
+            });
+        }
+
+        if (doc.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authorized',
+                error: { message: 'Technique created by other user' }
+            });
+        }
+
+        doc.percentage = req.body.percentage;
+        doc.name = req.body.name;
+        doc.save(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        });
+    });
+});
+
+
+
+
 
 module.exports = router;

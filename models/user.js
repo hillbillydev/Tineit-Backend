0e156de29c9b49f925f392afa40827dfdf4efdec
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-06 00:04:03 +0200

    Added apiKey propertie, and logic to remove everything.
    The logic removes every essential docs that bind to the user.

--- models/user.js
+++ models/user.js
@@ -2,15 +2,49 @@ var mongoose = require('mongoose');
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
+var User = require('./user');
+var Technique = require('./technique');
+var Company = require('./company');
+
 var schema = new Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    apiKey: {type: String, unique: true}
+    apiKey: { type: String, unique: true }
 });
 
 schema.plugin(mongooseUniqueValidator);
+
+schema.post('remove', function (doc) {
+    var deletedUser = doc;
+    Technique.find({ 'user': deletedUser._id }).exec(function (err, docs) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        docs.forEach(function (item) {
+            item.remove();
+        });
+    }).then(function () {
+
+        Company.find({ 'user': deletedUser._id }).exec(function (err, docs) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+
+            docs.forEach(function (item) {
+                item.remove();
+            });
+        });
+    });
+});
 
 schema.methods.toJSON = function () {
     var obj = this.toObject()

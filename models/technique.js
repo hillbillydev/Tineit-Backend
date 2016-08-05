455afc0bcc89d12438c109625ec1e97c2e1c6fe0
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 20:35:23 +0200

    Added unique validator to name.

--- models/technique.js
+++ models/technique.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
+var mongooseUniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 var scheme = new Schema({
-    name: { type: String, requried: true },
+    name: { type: String, requried: true, unique: true },
     percentage: { type: Number, requried: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', requird: true }
 });
+
+scheme.plugin(mongooseUniqueValidator);
 
 module.exports = mongoose.model('Technique', scheme);

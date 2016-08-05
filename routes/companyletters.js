c3d69f72e0039359bad726f9d0b074be6a8a9afc
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-06 01:14:09 +0200

    Post method for companyletters route.

---
+++ routes/companyletters.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonWebToken');
+
+var CompanyLetter = require('../models/companyLetter');
+var User = require('../models/user');
+
+
+router.post('/', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    User.findById(decoded.user._id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        if (!doc) {
+            return res.status(404).json({
+                title: 'User not found',
+                error: err
+            });
+        }
+
+        var companyLetter = new CompanyLetter({
+            text: req.body.text,
+            company: req.body.companyId
+        })
+
+        companyLetter.save(function (err, doc) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+
+            res.status(200).json({
+                title: 'Success',
+                obj: doc
+            });
+        });
+    })
+});

0ea9fcc4ab8ade0b7a9c1e1bddcbf278dd5378c2
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 08:27:53 +0200

    Company.

--- models/company.js
+++ models/company.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     name: { type: String, required: true },
     secret: { type: String, required: true },
-    user: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+    user: { type: Schema.Types.ObjectId, ref: 'User', requird: true }
 });
 
 module.exports = mongoose.model('Company', schema);

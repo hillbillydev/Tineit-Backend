986ca1400d033d624668961faceea527a96d3b4d
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-05 21:40:54 +0200

    Added delete to techniques route.

--- routes/techniques.js
+++ routes/techniques.js
@@ -5,6 +5,7 @@ var jwt = require('jsonWebToken');
 var Technique = require('../models/technique');
 var User = require('../models/user');
 
+
 router.post('/', function (req, res, next) {
     var decoded = jwt.decode(req.headers.authorization);
 
@@ -95,8 +96,44 @@ router.patch('/:id', function (req, res, next) {
     });
 });
 
+router.delete('/:id', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
 
+    Technique.findById(req.params.id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        
+        if (!doc) {
+            return res.status(404).json({
+                title: 'No technique found',
+                error: { message: 'Technique could not be found' }
+            });
+        }
 
+        if (doc.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authorized',
+                error: { message: 'Technique created by other user' }
+            });
+        }
 
+        doc.remove(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        })
+    });
+});
 
 module.exports = router;

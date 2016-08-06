d1929b539ac369f44c992748f8b95d095ae8b3a9
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-06 22:32:53 +0200

    Changed the model in company to have a letter propertie.

--- models/company.js
+++ models/company.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     name: { type: String, required: true },
     secret: { type: String, required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', requird: true }
+    user: { type: Schema.Types.ObjectId, ref: 'User', requird: true },
+    letter: {type: String, requird: true}
 });
 
 module.exports = mongoose.model('Company', schema);

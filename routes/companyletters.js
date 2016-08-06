619fc834630bf8a30e29a0e9518094ebf31c2c23
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-06 15:56:53 +0200

    Added delete and patch method to companyletter route.

--- routes/companyletters.js
+++ routes/companyletters.js
@@ -45,4 +45,84 @@ router.post('/', function (req, res, next) {
     })
 });
 
+router.delete('/:id', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    CompanyLetter.findById(req.params.id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        if (!doc) {
+            return res.status(404).json({
+                title: 'No companyLetter found',
+                error: { message: 'CompanyLetter could not be found' }
+            });
+        }
+
+        if (doc.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authorized',
+                error: { message: 'Companyletter was created by other user' }
+            });
+        }
+
+        doc.remove(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        })
+    });
+});
+
+router.patch('/:id', function (req, res, next) {
+    var decoded = jwt.decode(req.headers.authorization);
+
+    CompanyLetter.findById(req.params.id, function (err, doc) {
+        if (err) {
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!doc) {
+            return res.status(404).json({
+                title: 'No company letter found',
+                error: { message: 'CompanyLetter could not be found' }
+            });
+        }
+
+        if (doc.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authorized',
+                error: { message: 'Companyletter created by other user' }
+            });
+        }
+
+        doc.text = req.body.text;
+        doc.save(function (err, result) {
+            if (err) {
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        });
+    });
+});
+
 module.exports = router;

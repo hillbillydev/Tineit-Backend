6f65801a8a467110c5c58f074b7e0b3c17b7002c
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2016-08-06 22:33:01 +0200

    Removed company route.

--- models/companyLetter.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-    text: { type: String, required: true },
-    company: { type: Schema.Types.ObjectId, ref: 'Company', requird: true }
-});
-
-module.exports = mongoose.model('CompanyLetter', schema);
